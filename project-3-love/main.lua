0c610bd1a1c3c8ae9da1a4654589f03bfbc70147
Fernando Homem da Costa <nandohdc@gmail.com>
2021-06-08 20:44:10 -0300

    add buttons and text area

--- project-3-love/main.lua
+++ project-3-love/main.lua
@@ -1,4 +1,6 @@
-NUMBER_ARGUMENTS = 4
+
+
+NUMBER_ARGUMENTS = 1
 
 local configs = {}
 local buttons = {}
@@ -13,38 +15,48 @@ end
 function new_button(text, ftn)
     return {
         text = text,
-        ftn = ftn
+        ftn = ftn,
+        now = false,
+        last = false
     }
 end
 
 function check_config(arguments)
     if arguments ~= nil then
-        if #arguments == NUMBER_ARGUMENTS then
-            
+        if #arguments ~= NUMBER_ARGUMENTS then
+            logger("check_config", "Número de argumentos inválido.")
+            return false
+        else
+            return true
         end
-        logger("check_config", "Número de argumentos inválido.")
-        return false
     end
     logger("check_config", "Nenhum argumento foi fornecido.")
     return false
 end
 
-function logging(filename, message)
+function logging(filename, node_id, event)
     local file = nil
     file = assert(io.open(filename, "a+"))
-    file:write(os.date("%Y-%m-%d %H:%M:%S"), " - ", message)
+    file:write(os.date("%Y-%m-%d %H:%M:%S"), " , ", node_id, " , ", event)
     file:write("\n")
     file:close()
 end
 
 function love.load(arg)
-    local filename = "log"..os.date("%Y-%m-%d %H:%M:%S")..".txt"
-    logging(filename, "Oi")
+    local filename = "log"..os.date("%Y-%m-%d %H:%M:%S")..".csv"
+    -- logging(filename, "HASH1", "Oi")
     if check_config(arg) then
         font = love.graphics.newFont(32)
 
         table.insert(buttons, new_button(
-            "Text",
+            "Evento 1",
+            function ()
+                print("Ok")
+            end
+        ))
+
+        table.insert(buttons, new_button(
+            "Evento 2",
             function ()
                 print("Ok")
             end
@@ -65,19 +77,39 @@ function love.update(dt)
 end
 
 function love.draw()
-    local window_width = love.graphics.getWidth()
-    local window_height = love.graphics.getHeight()
+    local window_width = love.graphics.getWidth()/4
+    local window_height = love.graphics.getHeight()/2
     local button_height = 64
     local button_width = window_width * (1/3)
     local margin = 16
     local total_height = (button_height + 16) * #buttons
-
     local cursor_y = 0
+                          
 
     for i, button in ipairs(buttons) do
+
+        button.last = button.now
+
         local x = ((window_width * 0.5) - button_width * 0.5)
         local y = (window_height* 0.5) - (total_height * 0.5) + cursor_y
-        love.graphics.setColor(0.4, 0.4, 0.5, 1.0)
+
+        local color = {0.4, 0.4, 0.5, 1.0}
+
+        local mx, my = love.mouse.getPosition()
+
+        local hover_position =  mx > x and mx < x + button_width and my > y and my < y + button_height
+
+        if hover_position then
+            color = {0.8, 0.8, 0.9, 1.0}
+        end  
+        
+        button.now =  love.mouse.isDown(1)
+
+        if button.now and not button.last and hover_position then
+            button.ftn()
+        end
+
+        love.graphics.setColor(unpack(color))
         love.graphics.rectangle("fill",
         x,
         y,
@@ -86,12 +118,30 @@ function love.draw()
         )
 
         love.graphics.setColor(0, 0, 0, 1)
+        local textW = font:getWidth(button.text)
+        local textH = font:getHeight(button.text)
         love.graphics.print(
             button.text,
             font,
-            bx,
-            by
+            (window_width * 0.5) - textW * 0.5,
+            y + textH * 0.5
         )
         cursor_y = cursor_y + (button_height + margin)
+
+        color = {1, 1, 1, 1}
+
+        love.graphics.setColor(unpack(color))
+
+        
     end
+
+    love.graphics.rectangle("line",
+        ((window_width * 0.5) - button_width * 0.5) + 1.5*button_width,
+        (window_height* 0.5) - (total_height * 0.5),
+        button_width * 3.5,
+        button_height * 3.5)
+    
+    love.graphics.printf("OI.OI.OI.OI.OI.OI.OI.OI.OI.OI.", ((window_width * 0.5) - button_width * 0.5) + 1.5*button_width, (window_height* 0.5) - (total_height * 0.5), button_width * 3.5)
+
+
 end
